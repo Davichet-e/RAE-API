b76fe5935eb2bd565d3352a8f6723a28628749f6
David Garcia <davidgmorillop@gmail.com>
2020-01-02 11:34:23 +0100

    🔨 MInor changes

--- api_rae.py
+++ api_rae.py
@@ -1,31 +1,30 @@
 import json
 import re
-from collections import defaultdict
-from typing import Dict, Iterator, List, Union
+from typing import Dict, List, Optional, Union
 
 import bs4
 import requests
 
-BASE_URL = "https://dle.rae.es/"
+BASE_URL = "https://dle.rae.es/{word}"
 
 soup = bs4.BeautifulSoup(
-    requests.get(BASE_URL + input("Palabra a buscar: ")).content, "lxml"
+    requests.get(BASE_URL.format(word=input("Palabra a buscar: "))).content, "lxml"
 )
 
 results: bs4.Tag = soup.find("div", id="resultados")
 dicc: Dict[str, Dict[str, Union[str, List[str]]]] = {}
 
-for i, result in enumerate(results.find_all("article"), start=1):
-    meanings = result.find_all("p")
+for i, result in enumerate(results.find_all("article", recursive=False), start=1):
+    meanings: List[bs4.Tag] = result.find_all("p", recursive=False)
 
     dicc[i] = {}
-    complex_form = None
+    complex_form: str
     for p in meanings:
-        paragraph_class = p["class"][0]
-        
+        paragraph_class: str = p["class"][0]
+
         if re.fullmatch("j[0-9]?", paragraph_class):
             meaning_number, meaning_text = p.text.split(".", 1)
-            dicc[i][meaning_number] = meaning_text[1:]
+            dicc[i][meaning_number] = meaning_text.lstrip()
 
         elif paragraph_class in {"k5", "k6"}:
             complex_form = p.text
